4a981779000ca813b7578531a1141029803a6ad5
Nguyen Hoang Thi <nguyenhoangthikh2004@gmail.com>
2025-05-10 21:22:55 +0700

    doi import boostrap

--- index.php
+++ index.php
@@ -11,12 +11,13 @@
     <!-- Import Boxicons -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
+     <!-- Import Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <!-- Import CSS -->
     <link rel="stylesheet" href="style/mystyle.css">
     <link rel="stylesheet" href="style/mainstyle.css">
 
-     <!-- Import Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <title>Website Bán sách số 1 - VN</title>
 </head>
