bb3d96db5082c4e4e960cf811f78e421ccd2cad9
Thai <thailxag1122@gmail.com>
2025-05-10 13:57:17 +0700

    Them main(user & manager)

---
+++ page/main_user.php
@@ -0,0 +1,84 @@
+<div class="layout">
+  <div class="center">
+    <h2>📚 Danh sách sách</h2>
+
+    <div class="book-grid">
+      <div class="book-box">
+        <img src="img/sach1.jpg" alt="Đắc Nhân Tâm">
+        <h4>Đắc Nhân Tâm</h4>
+        <p>Tác giả: Dale Carnegie</p>
+        <button>Mua sách</button>
+      </div>
+
+      <div class="book-box">
+        <img src="img/sach2.jpg" alt="Nhà giả kim">
+        <h4>Nhà giả kim</h4>
+        <p>Tác giả: Paulo Coelho</p>
+        <button>Mua sách</button>
+      </div>
+
+      <div class="book-box">
+        <img src="img/sach3.jpg" alt="Tuổi trẻ đáng giá bao nhiêu">
+        <h4>Tuổi trẻ đáng giá bao nhiêu</h4>
+        <p>Tác giả: Rosie Nguyễn</p>
+        <button>Mua sách</button>
+      </div>
+
+      <div class="book-box">
+        <img src="img/sach3.jpg" alt="Tuổi trẻ đáng giá bao nhiêu">
+        <h4>Tuổi trẻ đáng giá bao nhiêu</h4>
+        <p>Tác giả: Rosie Nguyễn</p>
+        <button>Mua sách</button>
+      </div>
+
+      <div class="book-box">
+        <img src="img/sach3.jpg" alt="Tuổi trẻ đáng giá bao nhiêu">
+        <h4>Tuổi trẻ đáng giá bao nhiêu</h4>
+        <p>Tác giả: Rosie Nguyễn</p>
+        <button>Mua sách</button>
+      </div>
+
+      <div class="book-box">
+        <img src="img/sach3.jpg" alt="Tuổi trẻ đáng giá bao nhiêu">
+        <h4>Tuổi trẻ đáng giá bao nhiêu</h4>
+        <p>Tác giả: Rosie Nguyễn</p>
+        <button>Mua sách</button>
+      </div>
+
+      <div class="book-box">
+        <img src="img/sach3.jpg" alt="Tuổi trẻ đáng giá bao nhiêu">
+        <h4>Tuổi trẻ đáng giá bao nhiêu</h4>
+        <p>Tác giả: Rosie Nguyễn</p>
+        <button>Mua sách</button>
+      </div>
+
+      <div class="book-box">
+        <img src="img/sach3.jpg" alt="Tuổi trẻ đáng giá bao nhiêu">
+        <h4>Tuổi trẻ đáng giá bao nhiêu</h4>
+        <p>Tác giả: Rosie Nguyễn</p>
+        <button>Mua sách</button>
+      </div>
+
+      <div class="book-box">
+        <img src="img/sach3.jpg" alt="Tuổi trẻ đáng giá bao nhiêu">
+        <h4>Tuổi trẻ đáng giá bao nhiêu</h4>
+        <p>Tác giả: Rosie Nguyễn</p>
+        <button>Mua sách</button>
+      </div>
+
+      <!-- Thêm các ô sách tại đây -->
+    </div>
+  </div>
+
+  <div class="right">
+    <h3>🛒 Giỏ hàng</h3>
+    <ul>
+      <li>Đắc Nhân Tâm - 1 x 90.000đ</li>
+      <li>Nhà giả kim - 2 x 75.000đ</li>
+    </ul>
+    <hr />
+    <strong>Tổng: 240.000đ</strong>
+    <br /><br />
+    <button class="checkout">Thanh toán</button>
+  </div>
+</div>
